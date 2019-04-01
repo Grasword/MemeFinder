9993a09ae5f16b47842aaf9782e8ba3ac1419927
Oleg <Grasword6@gmail.com>
2019-04-01 17:22:56 +0300

    Create memeStorage.js

---
+++ src/memeStorage.js
@@ -0,0 +1,38 @@
+export const memeStorage = [
+  {
+    id: '1',
+    url: "https://i.imgur.com/jgDPoTy.png",
+    name: "Guess i`ll die",
+    tags: []
+  },
+  {
+    id: '2',
+    url: "https://i.imgur.com/Fnqgv49.png",
+    name: "Kirk",
+    tags: []
+  },
+  {
+    id: '3',
+    url: "https://i.imgur.com/WE4O315.jpg",
+    name: "Kirks and Spock",
+    tags: []
+  },
+  {
+    id: '4',
+    url: "https://i.imgur.com/TpbLzMY.png",
+    name: "MonkaS",
+    tags: []
+  },
+  {
+    id: '5',
+    url: "https://i.imgur.com/j7ixNpz.png",
+    name: "Not Bad",
+    tags: []
+  },
+  {
+    id: '6',
+    url: "https://i.imgur.com/qC4HMAu.png",
+    name: "Yao Ming",
+    tags: []
+  }
+];
