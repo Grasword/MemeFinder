6663d4690bc1032f2cd80332972fe384bc986730
Oleg <Grasword6@gmail.com>
2019-04-01 17:25:00 +0300

    add routs in router.js

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import FavoritesPage from "./components/Favorites.vue";
 import HomePage from "./components/Home.vue";
+import MemeDetails from "./components/memes/MemeDetails.vue";
 
 Vue.use(Router);
 
@@ -10,6 +11,8 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     { path: "/", name: "home", component: HomePage },
-    { path: "/favorites", name: "favorites", component: FavoritesPage }
+    { path: "/favorites", name: "favorites", component: FavoritesPage },
+    { path: "/meme/:id", props: true, name: "details", component: MemeDetails },
+    { path: "*", redirect: "/" }
   ]
 });
