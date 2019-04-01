3140d0df795e4260a85bb36b5ba95543dfaf3cf8
Oleg <Grasword6@gmail.com>
2019-04-01 17:22:16 +0300

    Create MemeDetails

---
+++ src/components/memes/MemeDetails.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <h2>{{meme.name}}</h2>
+        <img :src="meme.url">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { memeStorage } from "../../memeStorage.js";
+
+export default {
+  props: {
+    id: String
+  },
+  data() {
+    return {
+      meme: memeStorage.find(meme => meme.id === this.id)
+    };
+  }
+};
+</script>
+
+<style>
+</style>
