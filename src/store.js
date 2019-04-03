b8668ba3c5e93d8ce2eb1d7d886f207f289dee7a
Oleg <Grasword6@gmail.com>
2019-04-03 17:41:11 +0300

    Added functionality for favorites in Vuex store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,25 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    favorites: [""] //id
   },
   mutations: {
-
+    ADD_FAVORITE(state, id) {
+      state.favorites.push(id);
+    }
   },
   actions: {
-
+    addFavorite: ({ commit }, id) => {
+      commit("ADD_FAVORITE", id);
+    }
+  },
+  getters: {
+    getFavorites: state => {
+      return state.favorites;
+    }
   }
-})
+});
