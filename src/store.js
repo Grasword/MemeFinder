87e9db752e5404a5b8b15d36fa70adbcad23383b
Oleg <Grasword6@gmail.com>
2019-04-03 19:47:01 +0300

    add memeStorage in vuex store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { memeStorage } from "./memeStorage";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    favorites: [""] //id
+    favorites: [""],
+    memes: memeStorage
   },
   mutations: {
     ADD_FAVORITE(state, id) {
@@ -20,6 +22,9 @@ export default new Vuex.Store({
   getters: {
     getFavorites: state => {
       return state.favorites;
+    },
+    getMemes: state => {
+      return state.memes;
     }
   }
 });
