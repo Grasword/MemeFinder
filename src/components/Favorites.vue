0934f8a91f0fb7cddc203b5100d0320af397d46c
Oleg <Grasword6@gmail.com>
2019-04-03 17:34:31 +0300

    Create Favorites component

--- src/components/Favorites.vue
+++ src/components/Favorites.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
-    <h1>Тут Будут избранные</h1>
+    <div v-for="meme in memes" :key="meme.id">
+      {{meme.name}}
+      <img :src="meme.url">
+    </div>
   </div>
 </template>
+
+<script>
+import { memeStorage } from "../memeStorage.js";
+import { mapGetters } from "vuex";
+
+export default {
+  data() {
+    return {
+      memes: memeStorage.filter(meme =>
+        this.$store.getters.getFavorites.some(
+          favoriteId => favoriteId === meme.id
+        )
+      )
+    };
+  },
+  computed: {
+    ...mapGetters({
+      getFavorites: "getFavorites"
+    })
+  }
+};
+</script>
+
