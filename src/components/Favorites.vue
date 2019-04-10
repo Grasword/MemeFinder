750cd9d67df0fe7e21f13d400917746aa78f559c
Oleg <Grasword6@gmail.com>
2019-04-10 21:24:56 +0300

    add message if there are no favorites in Favorites component

--- src/components/Favorites.vue
+++ src/components/Favorites.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="wrapper">
     <div class="memes">
+      <div v-if="!memes.length" class="noFavorites">Избранных еще нет:(</div>
       <div v-for="meme in memes" :key="meme.id" class="memes__item">
         <p>{{meme.name}}</p>
         <img :src="meme.url">
@@ -43,10 +44,15 @@ img {
 .memes {
   display: flex;
   flex-flow: row wrap;
+  justify-content: center;
 }
 .memes__item {
   margin: 5px;
   flex: 1 1 32%;
+}
+.noFavorites {
+  margin-top: 50px;  
+  font-size: 26px;
 }
 </style>
 
