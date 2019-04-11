fc95470e575d9c40fef4ee1e8e7c6c1c0b6ff394
Oleg <Grasword6@gmail.com>
2019-04-11 19:59:26 +0300

    delete memeStorage import in favor of firebase in store.js

--- src/store.js
+++ src/store.js
@@ -1,13 +1,12 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { memeStorage } from "./memeStorage";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     favorites: [""],
-    memes: memeStorage
+    memes: [""]
   },
   mutations: {
     ADD_FAVORITE(state, id) {
