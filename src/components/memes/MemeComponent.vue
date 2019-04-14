e592b35d5fefd8823ff27c1b6e0dff92dc4cf5d3
Oleg <Grasword6@gmail.com>
2019-04-14 18:52:35 +0300

    delete search input to transfer it in Home.vue   / MemeComponent

--- src/components/memes/MemeComponent.vue
+++ src/components/memes/MemeComponent.vue
@@ -1,35 +1,19 @@
-<template>  
-    <div>
-      <input class="mainInput" type="text" v-model="filterText">
-      <div class="row">
-        <div v-for="meme in filteredMemes" :key="meme.id" class="col-xl-4">
-          <router-link :to="`/meme/${meme.id}`">
-            <img :src="meme.url">
-            <p>{{meme.name}}</p>
-          </router-link>
-        </div>
-      </div>
-    </div>  
+<template>
+  <div class="col-xl-4">
+    <router-link :to="`/meme/${meme.id}`">
+      <img :src="meme.url">
+      <p>{{meme.name}}</p>
+    </router-link>
+  </div>
 </template>
 
 <script>
 export default {
+  props: ["meme"],
   data() {
     return {
-      memes: [],
-      filterText: ""
+      memes: []
     };
-  },
-  computed: {
-    filteredMemes() {
-      return this.memes.filter(element => {
-        const regex = new RegExp(this.filterText, "i");
-        return regex.test(element.name);
-      });
-    }
-  },
-  created() {
-    this.memes = this.$store.getters.getMemes;
   }
 };
 </script>
